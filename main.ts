62ad59f530e74247326cc331a32486f61a40597b
T. Hamilton <profhamilton3@gmail.com>
2024-04-11 16:47:06 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
     if (0 == receivedNumber) {
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerDown), music.PlaybackMode.InBackground)
         maqueen.motorStop(maqueen.Motors.All)
@@ -17,20 +18,29 @@ radio.onReceivedNumber(function (receivedNumber) {
         maqueen.motorStop(maqueen.Motors.All)
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showNumber(group_id)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+})
 let dist = 0
-radio.setGroup(1)
+let group_id = 0
+group_id = 1
+radio.setGroup(group_id)
 radio.setFrequencyBand(31)
-basic.showNumber(1)
+basic.showNumber(group_id)
 maqueen.motorStop(maqueen.Motors.All)
 basic.forever(function () {
     dist = maqueen.Ultrasonic(PingUnit.Centimeters)
     if (dist < 5) {
         maqueen.motorStop(maqueen.Motors.All)
+        basic.showIcon(IconNames.Ghost)
         basic.pause(1000)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 25)
+        basic.showIcon(IconNames.Chessboard)
         basic.pause(1000)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 25)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
+        basic.showIcon(IconNames.Snake)
         basic.pause(1250)
     } else {
         basic.pause(200)
